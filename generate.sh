0a0cda80e714d9d5cd5521ccc768e109f4505bca
James Perretta <perretta.james@gmail.com>
2024-02-05 11:47:58 -0500

    Generated typed dict models from updated schema. Working on refactoring to use them where appropriate.

--- generate.sh
+++ generate.sh
@@ -1,12 +1,17 @@
 datamodel-codegen \
-    --input ag_contrib/config/generated/schema.yml \
+    --input src/ag_contrib/config/generated/schema.yml \
     --input-file-type openapi \
-    --output ag_contrib/config/generated/schema.py\
+    --output src/ag_contrib/config/generated/schema.py \
+    --output-model-type typing.TypedDict \
+    --disable-timestamp \
     --use-annotated \
     --use-generic-container-types \
     --use-standard-collections \
     --use-non-positive-negative-number-constrained-types \
+    --use-schema-description \
     --use-union-operator \
     --target-python-version 3.10 \
     --strip-default-none \
-    --strict-nullable
+    --strict-nullable \
+    --enum-field-as-literal one \
+    --use-subclass-enum
--- src/ag_contrib/config/models.py
+++ src/ag_contrib/config/models.py
@@ -1,17 +1,39 @@
-from enum import Enum
-from pydantic import BaseModel, ConfigDict, FilePath
+from __future__ import annotations
+
+from pathlib import Path
+from pydantic import BaseModel
 
 from ag_contrib.config.generated import schema as ag_schema
 
 
-class CourseData(BaseModel):
-    name: str
-    semester: ag_schema.Semester
-    year: int
+def _get_builtin_fdbk_presets():
+    return BUILTIN_FDBK_PRESETS
+
+
+class AGConfig(BaseModel):
+    project: ProjectConfig
+    feedback_presets: dict[str, ag_schema.AGTestCommandFeedbackConfig] = (
+        _get_builtin_fdbk_presets()
+    )
+    docker_images: dict[str, DockerImage] = {}
+
+
+class ProjectConfig(BaseModel):
+    settings: ag_schema.CreateProject
+    course: ag_schema.CreateCourse
+    student_files: list[ag_schema.ExpectedStudentFile] = []
+    instructor_files: list[InstructorFileConfig] = []
+    test_suites: list[TestSuiteConfig] = []
+
+
+class DockerImage(BaseModel):
+    build_dir: Path
+    include: list[Path] = []
+    exclude: list[Path] = []
 
 
 class InstructorFileConfig(BaseModel):
-    local_path: FilePath
+    local_path: Path
 
     @property
     def name(self) -> str:
@@ -24,143 +46,22 @@ class StudentFileConfig(BaseModel):
     max_num_matches: int = 1
 
 
-class StdinSource(Enum):
-    none = 'none'
-    text = 'text'
-    instructor_file = 'instructor_file'
-    setup_stdout = 'setup_stdout'
-    setup_stderr = 'setup_stderr'
-
-
-class ExpectedReturnCode(Enum):
-    none = 'none'
-    zero = 'zero'
-    nonzero = 'nonzero'
-
-
-class ExpectedOutputSource(Enum):
-    none = 'none'
-    text = 'text'
-    instructor_file = 'instructor_file'
-
-
-class ValueFeedbackLevel(Enum):
-    no_feedback = 'no_feedback'
-    correct_or_incorrect = 'correct_or_incorrect'
-    expected_and_actual = 'expected_and_actual'
-
-
-class AGTestCommandFeedbackConfig(BaseModel):
-    model_config = ConfigDict(use_enum_values=True)
-
-    visible: bool
-    return_code_fdbk_level: ValueFeedbackLevel
-    stdout_fdbk_level: ValueFeedbackLevel
-    stderr_fdbk_level: ValueFeedbackLevel
-    show_points: bool
-    show_actual_return_code: bool
-    show_actual_stdout: bool
-    show_actual_stderr: bool
-    show_whether_timed_out: bool
-
-
-BUILTIN_FDBK_PRESETS = {
-    'pass/fail': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stdout_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stderr_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        show_points=True,
-        show_actual_return_code=False,
-        show_actual_stdout=False,
-        show_actual_stderr=False,
-        show_whether_timed_out=False,
-    ),
-    'pass/fail+timeout': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stdout_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stderr_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        show_points=True,
-        show_actual_return_code=False,
-        show_actual_stdout=False,
-        show_actual_stderr=False,
-        show_whether_timed_out=True,
-    ),
-    'pass/fail+exit_status': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stdout_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stderr_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        show_points=True,
-        show_actual_return_code=True,
-        show_actual_stdout=False,
-        show_actual_stderr=False,
-        show_whether_timed_out=True,
-    ),
-    'pass/fail+output': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stdout_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stderr_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        show_points=True,
-        show_actual_return_code=False,
-        show_actual_stdout=True,
-        show_actual_stderr=True,
-        show_whether_timed_out=False,
-    ),
-    'pass/fail+diff': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.correct_or_incorrect,
-        stdout_fdbk_level=ValueFeedbackLevel.expected_and_actual,
-        stderr_fdbk_level=ValueFeedbackLevel.expected_and_actual,
-        show_points=True,
-        show_actual_return_code=False,
-        show_actual_stdout=False,
-        show_actual_stderr=False,
-        show_whether_timed_out=False,
-    ),
-    'private': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.no_feedback,
-        stdout_fdbk_level=ValueFeedbackLevel.no_feedback,
-        stderr_fdbk_level=ValueFeedbackLevel.no_feedback,
-        show_points=False,
-        show_actual_return_code=False,
-        show_actual_stdout=False,
-        show_actual_stderr=False,
-        show_whether_timed_out=False,
-    ),
-    'public': AGTestCommandFeedbackConfig(
-        visible=True,
-        return_code_fdbk_level=ValueFeedbackLevel.expected_and_actual,
-        stdout_fdbk_level=ValueFeedbackLevel.expected_and_actual,
-        stderr_fdbk_level=ValueFeedbackLevel.expected_and_actual,
-        show_points=True,
-        show_actual_return_code=True,
-        show_actual_stdout=True,
-        show_actual_stderr=True,
-        show_whether_timed_out=True,
-    )
-}
-
-
 class CommandConfig(BaseModel):
     name: str
     cmd: str
 
     stdin_source: StdinSource = StdinSource.none
-    stdin_text: str = ''
+    stdin_text: str = ""
     stdin_instructor_file: str | None = None
 
     expected_return_code: ExpectedReturnCode = ExpectedReturnCode.none
 
     expected_stdout_source: ExpectedOutputSource = ExpectedOutputSource.none
-    expected_stdout_text: str = ''
+    expected_stdout_text: str = ""
     expected_stdout_instructor_file: str | None = None
 
     expected_stderr_source: ExpectedOutputSource = ExpectedOutputSource.none
-    expected_stderr_text: str = ''
+    expected_stderr_text: str = ""
     expected_stderr_instructor_file: str | None = None
 
     ignore_case: bool = False
@@ -176,15 +77,15 @@ class CommandConfig(BaseModel):
     deduction_for_wrong_stdout: int = 0
     deduction_for_wrong_stderr: int = 0
 
-    normal_fdbk_config: AGTestCommandFeedbackConfig | str = 'pass/fail'
-    first_failed_test_normal_fdbk_config: AGTestCommandFeedbackConfig | str | None = None
-    ultimate_submission_fdbk_config: AGTestCommandFeedbackConfig | str = 'pass/fail'
-    past_limit_submission_fdbk_config: AGTestCommandFeedbackConfig | str = 'private'
-    staff_viewer_fdbk_config: AGTestCommandFeedbackConfig | str = 'public'
+    normal_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "pass/fail"
+    first_failed_test_normal_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str | None = None
+    ultimate_submission_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "pass/fail"
+    past_limit_submission_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "private"
+    staff_viewer_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "public"
 
     time_limit: int = 10
     use_virtual_memory_limit: bool = False
-    virtual_memory_limit: int = 500 * 10 ** 6
+    virtual_memory_limit: int = 500 * 10**6
     block_process_spawn: bool = False
 
     repeat: list[dict[str, object]] = []
@@ -205,18 +106,82 @@ class TestSuiteConfig(BaseModel):
     test_cases: list[TestCaseConfig] = []
 
 
-class ProjectSettings(BaseModel):
-    name: str
-
-
-class ProjectConfig(BaseModel):
-    settings: ProjectSettings
-    course: CourseData
-    student_files: list[StudentFileConfig] = []
-    instructor_files: list[InstructorFileConfig] = []
-    test_suites: list[TestSuiteConfig] = []
-
-
-class AGConfig(BaseModel):
-    project: ProjectConfig
-    feedback_presets: dict[str, AGTestCommandFeedbackConfig] = BUILTIN_FDBK_PRESETS
+BUILTIN_FDBK_PRESETS = {
+    "pass/fail": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="correct_or_incorrect",
+        stdout_fdbk_level="correct_or_incorrect",
+        stderr_fdbk_level="correct_or_incorrect",
+        show_points=True,
+        show_actual_return_code=False,
+        show_actual_stdout=False,
+        show_actual_stderr=False,
+        show_whether_timed_out=False,
+    ),
+    "pass/fail+timeout": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="correct_or_incorrect",
+        stdout_fdbk_level="correct_or_incorrect",
+        stderr_fdbk_level="correct_or_incorrect",
+        show_points=True,
+        show_actual_return_code=False,
+        show_actual_stdout=False,
+        show_actual_stderr=False,
+        show_whether_timed_out=True,
+    ),
+    "pass/fail+exit_status": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="correct_or_incorrect",
+        stdout_fdbk_level="correct_or_incorrect",
+        stderr_fdbk_level="correct_or_incorrect",
+        show_points=True,
+        show_actual_return_code=True,
+        show_actual_stdout=False,
+        show_actual_stderr=False,
+        show_whether_timed_out=True,
+    ),
+    "pass/fail+output": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="correct_or_incorrect",
+        stdout_fdbk_level="correct_or_incorrect",
+        stderr_fdbk_level="correct_or_incorrect",
+        show_points=True,
+        show_actual_return_code=False,
+        show_actual_stdout=True,
+        show_actual_stderr=True,
+        show_whether_timed_out=False,
+    ),
+    "pass/fail+diff": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="correct_or_incorrect",
+        stdout_fdbk_level="expected_and_actual",
+        stderr_fdbk_level="expected_and_actual",
+        show_points=True,
+        show_actual_return_code=False,
+        show_actual_stdout=False,
+        show_actual_stderr=False,
+        show_whether_timed_out=False,
+    ),
+    "private": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="no_feedback",
+        stdout_fdbk_level="no_feedback",
+        stderr_fdbk_level="no_feedback",
+        show_points=False,
+        show_actual_return_code=False,
+        show_actual_stdout=False,
+        show_actual_stderr=False,
+        show_whether_timed_out=False,
+    ),
+    "public": ag_schema.AGTestCommandFeedbackConfig(
+        visible=True,
+        return_code_fdbk_level="expected_and_actual",
+        stdout_fdbk_level="expected_and_actual",
+        stderr_fdbk_level="expected_and_actual",
+        show_points=True,
+        show_actual_return_code=True,
+        show_actual_stdout=True,
+        show_actual_stderr=True,
+        show_whether_timed_out=True,
+    ),
+}
