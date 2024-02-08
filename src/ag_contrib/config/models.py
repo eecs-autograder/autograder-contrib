39790a2c8d9dbddeaf9e1a647443df1f14150156
James Perretta <jameslp@umich.edu>
2024-02-08 09:26:58 -0500

    Added models for first config file test case types.

--- src/ag_contrib/config/models.py
+++ src/ag_contrib/config/models.py
@@ -1,19 +1,27 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Literal, TypeAlias
 from pydantic import BaseModel
 
 from ag_contrib.config.generated import schema as ag_schema
 
 
-def _get_builtin_fdbk_presets():
-    return BUILTIN_FDBK_PRESETS
+def _get_builtin_cmd_fdbk_presets():
+    return BUILTIN_CMD_FDBK_PRESETS
+
+
+def _get_builtin_test_suite_fdbk_presets():
+    return BUILTIN_TEST_SUITE_FDBK_PRESETS
 
 
 class AGConfig(BaseModel):
     project: ProjectConfig
     feedback_presets: dict[str, ag_schema.AGTestCommandFeedbackConfig] = (
-        _get_builtin_fdbk_presets()
+        _get_builtin_cmd_fdbk_presets()
+    )
+    feedback_presets_test_suite_setup: dict[str, ag_schema.AGTestSuiteFeedbackConfig] = (
+        _get_builtin_test_suite_fdbk_presets()
     )
     docker_images: dict[str, DockerImage] = {}
 
@@ -46,67 +54,189 @@ class StudentFileConfig(BaseModel):
     max_num_matches: int = 1
 
 
-class CommandConfig(BaseModel):
+class TestSuiteConfig(BaseModel):
+    name: str
+    instructor_files_needed: list[str] = []
+    read_only_instructor_files: bool = True
+    student_files_needed: list[str] = []
+
+    allow_network_access: bool = False
+    deferred: bool = False
+    sandbox_docker_image: str = 'Default'
+
+    setup_suite_cmd: str = 'echo "Configure your setup command here. Set to empty string to not use a setup command"'
+    setup_suite_cmd_name: str = 'Setup'
+    reject_submission_if_setup_fails: bool = False
+
+    normal_fdbk_config: str | ag_schema.AGTestSuiteFeedbackConfig = 'public'
+    ultimate_submission_fdbk_config: str | ag_schema.AGTestSuiteFeedbackConfig = 'public'
+    past_limit_submission_fdbk_config: str | ag_schema.AGTestSuiteFeedbackConfig = 'public'
+    staff_viewer_fdbk_config: str | ag_schema.AGTestSuiteFeedbackConfig = 'public'
+
+    test_cases: list[SingleCmdTestCaseConfig | MultiCmdTestCaseConfig] = []
+
+
+class TestCaseAdvancedFdbkConfig(BaseModel):
+    normal_fdbk_config: ag_schema.AGTestCaseFeedbackConfig = {
+        'visible': True,
+        'show_individual_commands': True,
+    }
+    ultimate_submission_fdbk_config: ag_schema.AGTestCaseFeedbackConfig = {
+        'visible': True,
+        'show_individual_commands': True,
+    }
+    past_limit_submission_fdbk_config: ag_schema.AGTestCaseFeedbackConfig = {
+        'visible': True,
+        'show_individual_commands': True,
+    }
+    staff_viewer_fdbk_config: ag_schema.AGTestCaseFeedbackConfig = {
+        'visible': True,
+        'show_individual_commands': True,
+    }
+
+
+class MultiCmdTestCaseConfig(BaseModel):
+    name: str
+    type: Literal['multi_cmd']
+    repeat: list[dict[str, object]] = []
+    advanced_feedback: TestCaseAdvancedFdbkConfig = TestCaseAdvancedFdbkConfig()
+    commands: list[MultiCommandConfig] = []
+
+    def do_repeat(self) -> list[MultiCmdTestCaseConfig]:
+        raise NotImplementedError
+
+    def to_json(self) -> ag_schema.AGTestCase:
+        raise NotImplementedError
+
+
+class MultiCommandConfig(BaseModel):
     name: str
     cmd: str
 
-    stdin_source: StdinSource = StdinSource.none
+    input: StdinSettings
+    return_code: MultiCmdReturnCodeCheckSettings
+    output_diff: MultiCmdDiffSettings
+    feedback: CommandFeedbackSettings
+    resources: ResourceLimits
+
+    repeat: list[dict[str, object]] = []
+
+
+class SingleCmdTestCaseConfig(BaseModel):
+    name: str
+    type: Literal['default', 'single_cmd']
+
+    input: StdinSettings
+    return_code: SingleCmdReturnCodeCheckSettings
+    output_diff: SingleCmdDiffSettings
+    feedback: CommandFeedbackSettings
+    resources: ResourceLimits
+
+    repeat: list[dict[str, object]] = []
+
+    def do_repeat(self) -> list[SingleCmdTestCaseConfig]:
+        raise NotImplementedError
+
+    def to_json(self) -> ag_schema.AGTestCase:
+        raise NotImplementedError
+
+
+class StdinSettings(BaseModel):
+    stdin_source: ag_schema.StdinSource = 'none'
     stdin_text: str = ""
     stdin_instructor_file: str | None = None
 
-    expected_return_code: ExpectedReturnCode = ExpectedReturnCode.none
 
-    expected_stdout_source: ExpectedOutputSource = ExpectedOutputSource.none
+class MultiCmdReturnCodeCheckSettings(BaseModel):
+    expected_return_code: ag_schema.ExpectedReturnCode = 'none'
+    points_for_correct_return_code: int = 0
+    deduction_for_wrong_return_code: int = 0
+
+
+class SingleCmdReturnCodeCheckSettings(BaseModel):
+    expected_return_code: ag_schema.ExpectedReturnCode = 'none'
+    points_for_correct_return_code: int = 0
+
+
+class MultiCmdDiffSettings(BaseModel):
+    expected_stdout_source: ag_schema.ExpectedOutputSource = 'none'
     expected_stdout_text: str = ""
     expected_stdout_instructor_file: str | None = None
+    points_for_correct_stdout: int = 0
+    deduction_for_wrong_stdout: int = 0
 
-    expected_stderr_source: ExpectedOutputSource = ExpectedOutputSource.none
+    expected_stderr_source: ag_schema.ExpectedOutputSource = 'none'
     expected_stderr_text: str = ""
     expected_stderr_instructor_file: str | None = None
+    points_for_correct_stderr: int = 0
+    deduction_for_wrong_stderr: int = 0
 
     ignore_case: bool = False
     ignore_whitespace: bool = False
     ignore_whitespace_changes: bool = False
     ignore_blank_lines: bool = False
 
-    points_for_correct_return_code: int = 0
+
+class SingleCmdDiffSettings(BaseModel):
+    expected_stdout_source: ag_schema.ExpectedOutputSource = 'none'
+    expected_stdout_text: str = ""
+    expected_stdout_instructor_file: str | None = None
     points_for_correct_stdout: int = 0
+
+    expected_stderr_source: ag_schema.ExpectedOutputSource = 'none'
+    expected_stderr_text: str = ""
+    expected_stderr_instructor_file: str | None = None
     points_for_correct_stderr: int = 0
 
-    deduction_for_wrong_return_code: int = 0
-    deduction_for_wrong_stdout: int = 0
-    deduction_for_wrong_stderr: int = 0
+    ignore_case: bool = False
+    ignore_whitespace: bool = False
+    ignore_whitespace_changes: bool = False
+    ignore_blank_lines: bool = False
 
+
+class CommandFeedbackSettings(BaseModel):
     normal_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "pass/fail"
     first_failed_test_normal_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str | None = None
     ultimate_submission_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "pass/fail"
     past_limit_submission_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "private"
     staff_viewer_fdbk_config: ag_schema.AGTestCommandFeedbackConfig | str = "public"
 
+
+class ResourceLimits(BaseModel):
     time_limit: int = 10
-    use_virtual_memory_limit: bool = False
-    virtual_memory_limit: int = 500 * 10**6
+    virtual_memory_limit: int | None = None
     block_process_spawn: bool = False
 
-    repeat: list[dict[str, object]] = []
+
+BUILTIN_TEST_SUITE_FDBK_PRESETS = {
+    'public': ag_schema.AGTestSuiteFeedbackConfig(
+        visible=True,
+        show_individual_tests=True,
+        show_setup_return_code=True,
+        show_setup_timed_out=True,
+        show_setup_stdout=True,
+        show_setup_stderr=True,
+    ),
+    'pass/fail': ag_schema.AGTestSuiteFeedbackConfig(
+        visible=True,
+        show_individual_tests=True,
+        show_setup_return_code=True,
+        show_setup_timed_out=True,
+        show_setup_stdout=False,
+        show_setup_stderr=False,
+    ),
+    'private': ag_schema.AGTestSuiteFeedbackConfig(
+        visible=True,
+        show_individual_tests=True,
+        show_setup_return_code=False,
+        show_setup_timed_out=False,
+        show_setup_stdout=False,
+        show_setup_stderr=False,
+    ),
+}
 
 
-class TestCaseConfig(BaseModel):
-    name: str
-    commands: list[CommandConfig] = []
-    # FIXME: sandbox and maybe other settings?
-
-    repeat: list[dict[str, object]] = []
-
-
-class TestSuiteConfig(BaseModel):
-    name: str
-    instructor_files_needed: list[str] = []
-    student_files_needed: list[str] = []
-    test_cases: list[TestCaseConfig] = []
-
-
-BUILTIN_FDBK_PRESETS = {
+BUILTIN_CMD_FDBK_PRESETS = {
     "pass/fail": ag_schema.AGTestCommandFeedbackConfig(
         visible=True,
         return_code_fdbk_level="correct_or_incorrect",
